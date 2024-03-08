151ab514074f04baa60451b39f9ef04e5573d5d9
Paola Nascimento <132537333+paolaalim@users.noreply.github.com>
2024-03-07 21:42:16 -0300

    Delete Projeto IV (Apadrihamento) directory

--- Projeto IV (Apadrihamento)/classes/material.py
+++
@@ -1,9 +0,0 @@
-class Material:
-    def __init__(self):
-        self.descricao = ""
-
-    def get_descricao(self):
-        return self.descricao
-
-    def set_descricao(self, descricao):
-        self.descricao = descricao
