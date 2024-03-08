f312b2d888d1ec73b8b687abc9a7ab6497f2670a
Paola Nascimento <132537333+paolaalim@users.noreply.github.com>
2024-03-07 21:39:43 -0300

    Delete Projeto Apadrinhamento directory

--- Projeto Apadrinhamento/classes/crianca.py
+++
@@ -1,6 +0,0 @@
-class Crianca:
-    def __init__(self, nome, idade, escola, interesses):
-        self.nome = nome
-        self.idade = idade
-        self.escola = escola
-        self.interesses = interesses
