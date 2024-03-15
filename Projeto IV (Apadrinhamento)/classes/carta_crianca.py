49ff9d39cffc03b2a1b21532d1a422486f438e60
Paola Nascimento <132537333+paolaalim@users.noreply.github.com>
2024-03-15 17:48:01 -0300

    Update carta_crianca.py

--- Projeto IV (Apadrinhamento)/classes/carta_crianca.py
+++ Projeto IV (Apadrinhamento)/classes/carta_crianca.py
@@ -1,6 +1,6 @@
 from classes.crianca import Crianca
 
-class CartaCrianca:
+class CartaCrianca(Crianca):
     def __init__(self, nome, idade, mensagem):
         super()._init_ (nome, idade)        
         self.mensagem = mensagem
