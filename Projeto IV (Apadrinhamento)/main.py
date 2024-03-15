d692be6bad243b01139193cef89ee7a349f4b634
Paola Nascimento <132537333+paolaalim@users.noreply.github.com>
2024-03-15 17:38:52 -0300

    Update main.py

--- Projeto IV (Apadrinhamento)/main.py
+++ Projeto IV (Apadrinhamento)/main.py
@@ -16,6 +16,14 @@ def main():
     print("\nCadastro como padrinho:")
     usuario.cadastrar_como_padrinho()
 
+    # Exemplo adicional de funcionalidade (buscar criança)
+    print("\nBusca por uma criança para apadrinhar:")
+    padrinho = Padrinho()
+    crianca_apadrinhada = padrinho.buscar_crianca()
+    if crianca_apadrinhada:
+        print("Criança apadrinhada:", crianca_apadrinhada.nome)
+    else:
+        print("Não há crianças disponíveis para adoção.")
+
 if __name__ == "__main__":
     main()
-
