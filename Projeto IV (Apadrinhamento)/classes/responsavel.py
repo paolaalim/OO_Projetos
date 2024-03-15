5402e5ebef4cc3e538cec559b41693da7d15da2a
Paola Nascimento <132537333+paolaalim@users.noreply.github.com>
2024-03-15 18:04:50 -0300

    Update responsavel.py

--- Projeto IV (Apadrinhamento)/classes/responsavel.py
+++ Projeto IV (Apadrinhamento)/classes/responsavel.py
@@ -3,7 +3,6 @@ from classes.usuario import Usuario
 class Responsavel(Usuario):
     def __init__(self, nome, email, senha, telefone, endereco, criancas):
         super()._init_ (nome, email, senha, telefone, endereco)   
-
         self.criancas = []
 
     def cadastrar_crianca(self, crianca):
