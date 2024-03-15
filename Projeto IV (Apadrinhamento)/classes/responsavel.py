fe2423c59e613c25106888604f02c20c567da28f
Paola Nascimento <132537333+paolaalim@users.noreply.github.com>
2024-03-15 17:40:31 -0300

    Update responsavel.py

--- Projeto IV (Apadrinhamento)/classes/responsavel.py
+++ Projeto IV (Apadrinhamento)/classes/responsavel.py
@@ -1,14 +1,3 @@
-class CartaCrianca:
-    def __init__(self, nome, idade, mensagem):
-        self.nome = nome
-        self.idade = idade
-        self.mensagem = mensagem
-
-class Crianca:
-    def __init__(self, nome, idade):
-        self.nome = nome
-        self.idade = idade
-
 class Responsavel:
     def __init__(self):
         self.criancas = []
